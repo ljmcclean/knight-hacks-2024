833d9e0052215139794f160698f9c1ee2cb14787
nwlehman <na578283@ucf.edu>
2024-10-05 15:08:03 -0400

    Gets rid of profile button in discover page

--- server/public/js/discover.js
+++ server/public/js/discover.js
@@ -3,16 +3,35 @@ const projs = document.querySelector("#projs");
 
 const skillInput = document.querySelector("#skill-inp");
 const addSkillBtn = document.querySelector("#add-skill-btn");
-addSkillBtn.addEventListener("click", function(){
-    console.log(14);
-});
+const skillList = document.querySelector("#profile-skill-list");
+
 
 const searchInput = document.querySelector("#discover-search");
 const searchBtn = document.querySelector("#search-btn");
 
-let skills = [];
+let skills = ["skill one", "skill two", "three"];
 
+const addListItem = () => {
+    if(skillInput.value !== ""){
+        skills.push(skillInput.value);
+    }
+    skillInput.value = "";
+    console.log("This is in add list item");
+}
 
+const renderSkills = () => {
+    skillList.innerHTML = "";
+
+    for(let i = 0; i < skills.length; i++){
+        skillList.innerHTML += `<li>${skills[i]}</li>`;
+    }
+    console.log("this is in renderSkills")
+}
+
+addSkillBtn.addEventListener("click", function(){
+    addListItem();
+    renderSkills();
+});
 
 const renderPosts = () => {
     fetch('/api/project/1')
