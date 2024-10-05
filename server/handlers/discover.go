c0c62da65086e00543649b245987968bd4f46870
Lucas McClean <LucksDeveloper@gmail.com>
2024-10-04 23:05:09 -0400

    feat: make discover available

---
+++ server/handlers/discover.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"context"
+	"knight-hacks-2024/server/templates"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
+// Protected route with session passed through via context
+func GetDiscover(ctx context.Context) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(templates.Discover()).ServeHTTP(w, r)
+	})
+}
